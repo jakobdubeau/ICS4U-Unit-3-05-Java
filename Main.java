8cc3fe425de9a05bd991b5fa94cafb39218c3d11
Ubuntu <ubuntu@ip-172-31-82-105.ec2.internal>
2022-01-16 04:37:16 +0000

    done

--- Main.java
+++ Main.java
@@ -7,9 +7,12 @@
 *
 */
 
+/**
+* Main.
+*/
 final class Main {
 
-/**
+    /**
     * Prevents instantiation.
     *
     *  @throws IllegalStateException
@@ -21,16 +24,45 @@ final class Main {
     }
 
     /**
-    * The vehicle program.
+    * This is the main function.
     *
-    * @param args No args will be used
+    * @param args this will not be used
     */
-
     public static void main(final String[] args) {
 
-        final Vehicle car = new Vehicle("AAAA", "red", 4, 50);
-        System.out.println(car.getColour());
-        car.setColour("blue");
-        System.out.println(car.getColour());
+        final int fifty = 50;
+        final String period = ".";
+        final int sixty = 60;
+        final int twenty = 20;
+
+        final Vehicle car = new Vehicle("CB7AX9", "Red", 4, 50);
+        final Vehicle car2 = new Vehicle("5KAJE6", "Blue", 6, 80);
+
+        System.out.println("\nThe max speed of the both the cars are "
+            + car.getMaxSpeed() + " and " + car2.getMaxSpeed() + period);
+
+        System.out.println("\nThe first car's colour is " + car.getColour()
+            + period);
+        car.setColour("Purple");
+        System.out.println("\nThe first car's colour changed to "
+            + car.getColour() + period);
+
+        car.accelerate(fifty);
+        System.out.println("\nCar 1 has had their speed accelerated to "
+            + car.getSpeed() + period);
+
+        System.out.println("\nThe second car's plate is " + car2.getLiscence()
+            + period);
+        car2.setLiscence("AAAAAA");
+        System.out.println("\nThe second car's plate changed to "
+            + car2.getLiscence() + period);
+
+        car2.accelerate(sixty);
+        System.out.println("\nCar 2 has had their speed accelerated to "
+            + car2.getSpeed() + period);
+
+        car2.brake(twenty);
+        System.out.println("\nCar2 hit the brake to make their speed "
+            + car2.getSpeed());
     }
 }
--- Vehicle.java
+++ Vehicle.java
@@ -11,45 +11,146 @@
 */
 public final class Vehicle {
 
+    /**
+    * Plate.
+    */
     private String liscensePlate;
 
+    /**
+    * Colour.
+    */
     private String colour;
 
+    /**
+    * Doors.
+    */
     private int doors;
 
+    /**
+    * Speed.
+    */
     private int speed;
 
+    /**
+    * Max Speed.
+    */
     private int maxSpeed;
+
+    /**
+    * Initial values.
+    *
+    * @param startColour colour
+    * @param startPlate plate
+    * @param startDoors door
+    * @param vehicleMaxSpeed speed
+    */
 
     public Vehicle(final String startPlate, final String startColour, final int
                     startDoors, final int vehicleMaxSpeed) {
 
-    this.liscensePlate = startPlate;
+        this.liscensePlate = startPlate;
 
-    this.colour = startColour;
+        this.colour = startColour;
 
-    this.doors = startDoors;
+        this.doors = startDoors;
 
-    this.speed = 0;
+        this.speed = 0;
 
-    this.maxSpeed = vehicleMaxSpeed;
+        this.maxSpeed = vehicleMaxSpeed;
 
-}
+    }
 
+    /**
+    * Getter.
+    *
+    * @return return
+    */
     public String getLiscence() {
         return this.liscensePlate;
     }
 
+    /**
+    * Getter.
+    *
+    * @return return
+    */
     public String getColour() {
         return this.colour;
     }
 
-    //setters
+    /**
+    * Getter.
+    *
+    * @return return
+    */
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    /**
+    * Getter.
+    *
+    * @return return
+    */
+    public int getDoors() {
+        return this.doors;
+    }
+
+    /**
+    * Getter.
+    *
+    * @return return
+    */
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    // setters
+
+    /**
+    * Setter.
+    *
+    * @param someLiscence Liscence
+    */
     public void setLiscence(String someLiscence) {
         this.liscensePlate = someLiscence;
     }
 
+    /**
+    * Setter.
+    *
+    * @param someColour Colour
+    */
     public void setColour(String someColour) {
         this.colour = someColour;
+    }
+
+    /**
+    * Accelerates.
+    *
+    * @param acceleration acceleration.
+    */
+    public void accelerate(final int acceleration) {
+        if (acceleration + speed > maxSpeed) {
+            System.out.println("\nCannot accelerate more"
+                + " than the maximum speed");
+        }
+        else {
+            speed = speed + acceleration;
+        }
+    }
+
+    /**
+    * Applies brakes to a car.
+    *
+    * @param brakes the amount to which the speed will be reduced
+    */
+    public void brake(final int brakes) {
+        if (brakes > speed) {
+            System.out.println("\nCannot brake more than the current speed");
+        }
+        else {
+            speed = speed - brakes;
+        }
     }
 }
